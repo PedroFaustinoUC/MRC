63a2121708f8b4c17652a3b8a1eec3bd2bf979cd
Pedro Faustino <pfaustino.pt@gmail.com>
2013-10-04 23:53:57 +0100

    Serviços de atendimento são carregados da base de dados Restantes selects ficam bloqueados para que a escolha seja sequencial

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -3,18 +3,15 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+    
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     
-     // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+    /*
+     * @Função executada assim que a APP inicia
+     */
     onDeviceReady: function() {
     	//Configurações para suporte do PhoneGap
     	//http://view.jquerymobile.com/1.3.2/dist/demos/faq/how-configure-phonegap-cordova.html
@@ -24,10 +21,82 @@ var app = {
 	    $.mobile.allowCrossDomainPages = true;
 	    $.mobile.defaultPageTransition='none';
 	    $.mobile.defaultDialogTransition='none';
+
+	    /*
+	     * @Declaração de variáveis globais
+	     */
 	    //Variável que armazena o identificador unico do telemovel
-	    //Get the device's Universally Unique Identifier (UUID).
 	    var uniqueID = device.uuid;
+	    //URL base do webservice
+	    var rootURL = "http://10.3.3.126/mrc/api/";
+	    
+	    /*
+	     * @Declaração de Funções
+	     */
+	    //Webservice - carregamento da lista de serviços de atendimento
+	    function getFrontDesk(){
+		    $.ajax({
+				type: 'GET',
+				url: rootURL+"frontdesk",
+				dataType: "json", // data type of response
+				success: function(data){
+					$('#frontdesk').empty();
+					var list = data == null ? [] : (data.frontDesk instanceof Array ? data.frontDesk : [data.frontDesk]);
+				    $('#frontdesk').append($('<option>').text(""));
+					$.each(list, function(index, frontDesk) {
+			            $('#frontdesk').append($('<option>').text(frontDesk.nome).attr('value', frontDesk.idFrontDesk));
+					});
+					$( "#frontdesk" ).selectmenu( "refresh" );
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
+	    
+	  //Webservice - carregamento da localização do serviço de atendimento
+	    function getLocalizacao(id){
+		    $.ajax({
+				type: 'GET',
+				url: rootURL+"frontdesk",
+				dataType: "json", // data type of response
+				success: function(data){
+					$('#frontdesk').empty();
+					var list = data == null ? [] : (data.frontDesk instanceof Array ? data.frontDesk : [data.frontDesk]);
+				    $('#frontdesk').append($('<option>').text(""));
+					$.each(list, function(index, frontDesk) {
+			            $('#frontdesk').append($('<option>').text(frontDesk.nome).attr('value', frontDesk.idFrontDesk));
+					});
+					$( "#frontdesk" ).selectmenu( "refresh" );
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
 
-
+	    /* ____________
+	     * |IMPORTANTE|
+	     *  ----------
+	     * @Ecrã inicial da APP corresponde ao menu "Gerar Senha"
+	     * É necessário carregar a lista de Serviços de Atendimento assim que a APP inicia
+	     */
+	    getFrontDesk();
+	    
+	    /*
+	     * @Declaração de Eventos
+	     */   
+	    //Cada acesso à página "Gerar Ticket" requer uma chamada ao webservice correspondente
+	    //Isto permite ter os dados actualizados
+	    $("#gerar_senha_page").on( "pageshow", getFrontDesk());
+	    
+	    //Quando o valor do select "Serviço de Atendimento" for alterado, o select "Localização" fica activo
+	    $("#frontdesk").on("change",function(){
+	    	$("#localizacao").selectmenu( "enable" );
+	    	getLocalizacao(id)
+	    })
+	    
+	    
+	    
     }
 };
