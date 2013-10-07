d18950100ce5e0f606e3b917d154782d23e13115
Pedro Faustino <pfaustino.pt@gmail.com>
2013-10-07 13:08:58 +0100

    Calculo das médias de tempos concluido

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -6,11 +6,19 @@ var app = {
     
     // Bind Event Listeners
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    	document.addEventListener('backbutton', this.onBackKeyDown, false);
+    	document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     
     /*
-     * @Função executada assim que a APP inicia
+     * @Função executada quando se clica na tecla de retroceder
+     */
+    onBackKeyDown: function() {
+		navigator.app.exitApp();
+	},
+    
+    /*
+     * @Função executada quando a APP inicia
      */
     onDeviceReady: function() {
     	//Configurações para suporte do PhoneGap
@@ -21,6 +29,9 @@ var app = {
 	    $.mobile.allowCrossDomainPages = true;
 	    $.mobile.defaultPageTransition='none';
 	    $.mobile.defaultDialogTransition='none';
+	    
+
+		
 
 	    /*
 	     * @Declaração de variáveis globais
@@ -28,12 +39,13 @@ var app = {
 	    //Variável que armazena o identificador unico do telemovel
 	    var uniqueID = device.uuid;
 	    //URL base do webservice
-	    var rootURL = "http://10.3.3.126/mrc/api/";
+	    //var rootURL = "http://10.3.3.126/mrc/api/";
+	    var rootURL = "http://192.168.1.64/mrc/api/";
 	    
 	    /*
 	     * @Declaração de Funções
 	     */
-	    //Webservice - carregamento da lista de serviços de atendimento
+	    //Webservice - carregamento dos serviços de atendimento
 	    function getFrontDesk(){
 		    $.ajax({
 				type: 'GET',
@@ -54,20 +66,61 @@ var app = {
 			});
 		    }
 	    
-	  //Webservice - carregamento da localização do serviço de atendimento
+	  //Webservice - carregamento das localizações 
 	    function getLocalizacao(id){
 		    $.ajax({
 				type: 'GET',
-				url: rootURL+"frontdesk",
+				url: rootURL+"frontdesk/"+id+"/localizacao",
 				dataType: "json", // data type of response
 				success: function(data){
-					$('#frontdesk').empty();
-					var list = data == null ? [] : (data.frontDesk instanceof Array ? data.frontDesk : [data.frontDesk]);
-				    $('#frontdesk').append($('<option>').text(""));
-					$.each(list, function(index, frontDesk) {
-			            $('#frontdesk').append($('<option>').text(frontDesk.nome).attr('value', frontDesk.idFrontDesk));
+					$('#localizacao').empty();
+					var list = data == null ? [] : (data.localizacao instanceof Array ? data.localizacao : [data.localizacao]);
+				    $('#localizacao').append($('<option>').text(""));
+					$.each(list, function(index, localizacao) {
+			            $('#localizacao').append($('<option>').text(localizacao.nome).attr('value', localizacao.idLocalizacao));
 					});
-					$( "#frontdesk" ).selectmenu( "refresh" );
+					$( "#localizacao" ).selectmenu( "refresh" );
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
+	    
+	  //Webservice - carregamento dos balcões de antedimento
+	    function getBalcaoAtendimento(idFrontDesk,idLocalizacao){
+		    $.ajax({
+				type: 'GET',
+				url: rootURL+"frontdesk/"+idFrontDesk+"/localizacao/"+idLocalizacao+"/balcaoatendimento",
+				dataType: "json", // data type of response
+				success: function(data){
+					$('#balcaoAtendimento').empty();
+					var list = data == null ? [] : (data.balcaoAtendimento instanceof Array ? data.balcaoAtendimento : [data.balcaoAtendimento]);
+				    $('#balcaoAtendimento').append($('<option>').text(""));
+					$.each(list, function(index, balcaoAtendimento) {
+			            $('#balcaoAtendimento').append($('<option>').text(balcaoAtendimento.nome).attr('value', balcaoAtendimento.idbalcaoatendimento));
+					});
+					$( "#balcaoAtendimento" ).selectmenu( "refresh" );
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
+	    
+	  //Webservice - verificar senha actual e tempos
+	    function getCurrentSenha(idFrontDesk,idLocalizacao,idBalcaoAtendimento){
+		    $.ajax({
+				type: 'GET',
+				url: rootURL+"frontdesk/"+idFrontDesk+"/localizacao/"+idLocalizacao+"/balcaoatendimento/"+idBalcaoAtendimento,
+				dataType: "json", // data type of response
+				success: function(data){
+					$('#tempo_espera').val(data.tempoEspera+" minutos");
+					$('#tempo_atendimento').val(data.tempoAtendido+" minutos");
+					$('#hora_prevista').val(data.horaPrevista);
+					$('#senhaActual').empty();
+		            $('#senhaActual').siblings('.ui-btn-inner').children('.ui-btn-text').text("Senha Actual: "+data.senha);
+					$( "#senhaActual" ).buttonMarkup( "refresh" );
 				},
 				error: function(jqXHR, textStatus, errorThrown){
 					alert('Erro: ' + textStatus);
@@ -90,13 +143,33 @@ var app = {
 	    //Isto permite ter os dados actualizados
 	    $("#gerar_senha_page").on( "pageshow", getFrontDesk());
 	    
-	    //Quando o valor do select "Serviço de Atendimento" for alterado, o select "Localização" fica activo
+	    //Quando o select "Serviço de Atendimento" for alterado o select "Localização" fica activo
+	    //Após o select "Localizacao" ficar activo, são carregados os dados
 	    $("#frontdesk").on("change",function(){
 	    	$("#localizacao").selectmenu( "enable" );
-	    	getLocalizacao(id)
+	    	getLocalizacao($("#frontdesk").val());
 	    })
 	    
+	    //Quando o select "Localização" for alterado o select "Balcao Atendimento" fica activo
+	    //Após o select "Balcao Atendimento" ficar activo, são carregados os dados
+	    $("#localizacao").on("change",function(){
+	    	$("#balcaoAtendimento").selectmenu( "enable" );
+	    	getBalcaoAtendimento($("#frontdesk").val(),$("#localizacao").val());
+	    })
 	    
+	    //Quando o select "Balcao Atendimento" for alterado o botão "Avançar" fica activo
+	    $("#balcaoAtendimento").on("change",function(){
+	    	$("#gerar_senha_page_avancarButton").removeClass( "ui-disabled" );
+	    })
+	    
+	    //Quando o select "Balcao Atendimento" for alterado o botão "Avançar" fica activo
+	    $("#info_gerar_senha_page").on("pagebeforeshow",function(){
+	    	getCurrentSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val());
+	    })
+	    
+	    $("#senhaActual").on("click",function(){
+	    	getCurrentSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val());
+	    })
 	    
     }
 };
