46b6a143306e57d78eccd6d650d94da30880855a
Pedro Faustino <pfaustino.pt@gmail.com>
2013-10-14 18:05:12 +0100

    Grande update

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -22,8 +22,14 @@ var app = {
 	    $.mobile.allowCrossDomainPages = true;
 	    $.mobile.defaultPageTransition='none';
 	    $.mobile.defaultDialogTransition='none';
+	    
+	    var pushNotification = window.plugins.pushNotification;
+	    pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"497143846050","ecb":"app.onNotificationGCM"});
+	    
 	    //Listener da tecla de retroceder
     	document.addEventListener("backbutton", onBackKeyDown, false);
+    	
+    	document.addEventListener("offline", onOffline, false);
 
 	    /*
 	     * @Declaração de variáveis globais
@@ -31,7 +37,10 @@ var app = {
 	    //Variável que armazena o identificador unico do telemovel
 	    var uniqueID = device.uuid;
 	    //URL base do webservice
-	    var rootURL = "http://10.3.3.126/mrc/api/";
+	    //var rootURL = "http://10.3.3.126/mrc/api/";
+	    var rootURL = "http://10.3.1.39/mrc/api/";
+	    var gmaps ="";
+	    var idSenhaPopUp = 0;
 	    //var rootURL = "http://192.168.1.64/mrc/api/";
 	    
 	    /*
@@ -40,8 +49,25 @@ var app = {
 	    
 	    //Quando se carrega na tecla de retroceder a aplicação fecha
 	    function onBackKeyDown() {
-			navigator.app.exitApp();
+	    	//playBeep()
+	    	//vibrate()
+	    	//navigator.app.exitApp();
 		};
+		
+		//Alerta que surge quando se desiste de uma senha
+		function showAlertDesistir() {
+	        navigator.notification.alert(
+	            'Senha removida!',  // message
+	            null,         // callback
+	            'Desistir da Senha',   // title
+	            'Ok'                  // buttonName
+	        );};
+	        
+	    //Evento da ligação à internet perdida
+		function onOffline(){
+			alert("Sem ligação à internet!");
+		};
+		
 	    //Webservice - carregamento dos serviços de atendimento
 	    function getFrontDesk(){
 		    $.ajax({
@@ -125,12 +151,12 @@ var app = {
 			});
 		    }
 	    
+		//Webservice - gerar senha
 	    function addSenha(frontDesk, localizacao,balcaoAtendimento,uniqueID) {
-	    	alert("entrou");
 	    	$.ajax({
 	    		type: 'POST',
 	    		contentType: 'application/json',
-	    		url: rootURL+"senha",
+	    		url: rootURL+"senhas",
 	    		dataType: "json",
 	    		data: JSON.stringify({
 	    			"frontDesk": frontDesk, 
@@ -139,13 +165,99 @@ var app = {
 	    			"uniqueID": uniqueID
 	    			}),
 	    		success: function(data, textStatus, jqXHR){
-	    			alert("sucesso");
+	    			getSenhaUtilizador(data.id);
+	    			$.mobile.changePage("#popupSenhaActual",{ role: "dialog" } );
 	    		},
 	    		error: function(jqXHR, textStatus, errorThrown){
 	    			alert('Erro: ' + textStatus);
 	    		}
 	    	});
 	    }
+	    
+	  //Webservice - verificar senha gerada pelo utilizador
+	    function getSenhaUtilizador(idsenha){
+		    $.ajax({
+				type: 'GET',
+				url: rootURL+"senhas/"+idsenha,
+				dataType: "json", // data type of response
+				success: function(data){
+					$('#dialog_senhaActual').empty();
+		            $('#dialog_senhaActual').siblings('.ui-btn-inner').children('.ui-btn-text').text("Senha: "+data.senha);
+					$( "#dialog_senhaActual" ).buttonMarkup( "refresh" );
+					$('#dialog_hora_prevista').val(data.horaPrevista);
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
+	    
+	  //Webservice - verificar senhas do utilizador
+	    function getSenhasUtilizador(uniqueID){
+		    $.ajax({
+				type: 'GET',
+				url: rootURL+"utilizador/"+uniqueID,
+				dataType: "json", // data type of response
+				success: function(data){
+					$('#listaMinhasSenhas').empty();
+					var list = data == null ? [] : (data.senhas instanceof Array ? data.senhas : [data.senhas]);
+					if(data.senhas.length==0)
+						{
+						$('#minhasSenhasRefresh').empty();
+			            $('#minhasSenhasRefresh').siblings('.ui-btn-inner').children('.ui-btn-text').text("Não existem senhas");
+						$( "#minhasSenhasRefresh" ).buttonMarkup( "refresh" );
+						}
+					else 
+					{
+						$('#minhasSenhasRefresh').empty();
+			            $('#minhasSenhasRefresh').siblings('.ui-btn-inner').children('.ui-btn-text').text("Actualizar");
+						$( "#minhasSenhasRefresh" ).buttonMarkup( "refresh" );
+						}
+					var idControlo = 0;
+					var lista = "";				
+					$.each(list, function(index, senhas) {
+						if(idControlo != senhas.idFrontDesk){
+						lista+= '<li data-role="list-divider">'+senhas.nomeFrontDesk+'</li>';
+						idControlo=senhas.idFrontDesk
+						}
+						lista+=('<li data-gmaps="'+senhas.googleMaps+'" data-idSenha="'+senhas.idSenha+'"><a href="#popupsenha" data-rel="popup"><h2>'+senhas.nomeBalcaoAtendimento+'</h2><p><strong>'+senhas.nomeLocalizacao+'</strong></p><br>');
+						if((senhas.nSenhaUtilizador-senhas.nSenhaActual)<=5)
+						{
+							lista+=('<p><font color="red"><strong>Senha Actual: </strong>'+senhas.nSenhaActual+'<strong> Hora Prevista: </strong>'+senhas.horaAtendimento+'</font></p>');
+						}
+						else
+						lista+=('<p><strong>Senha Actual: </strong>'+senhas.nSenhaActual+'<strong> Hora Prevista: </strong>'+senhas.horaAtendimento+'</p>');
+						
+						lista+=('<p><strong>Minha Senha: </strong>'+senhas.nSenhaUtilizador+'</p></a></li>')
+					});
+					$('#listaMinhasSenhas').append(lista);
+					$( "#listaMinhasSenhas" ).listview( "refresh" );
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
+	    
+	  //Webservice - desistir da senha
+	    function giveUpSenha(idSenhaPopUp){
+		    $.ajax({
+				type: 'PUT',
+				url: rootURL+"senhas/"+idSenhaPopUp,
+				dataType: "json", // data type of response
+				success: function(data){
+					//$.mobile.changePage("#minhas_senhas_page" );
+					$( "#popupsenha" ).popup( "close" );
+					showAlertDesistir();
+					$("#minhas_senhas_page").trigger("pageshow");
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
+		
+				
 
 	    /* ____________
 	     * |IMPORTANTE|
@@ -199,12 +311,68 @@ var app = {
 	    	getCurrentSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val());
 	    })
 	    
+	    //Quando se clica no botão da senha actual a página é actualizada 
 	    $("#senhaActual").on("click",function(){
 	    	getCurrentSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val());
 	    })
 	    
+	    //Quando se clica no botão de gerar ticket é gerada uma nova senha
 	    $("#info_gerar_senha_page_gerarTicketButton").on("click",function(){
 	    	addSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val(),uniqueID);
 	    })
+	    
+	    //Quando a página das senhas é carregada 
+	    $("#minhas_senhas_page").on("pageshow",function(){
+	    	getSenhasUtilizador(uniqueID);
+	    })
+	    
+	   $("#minhasSenhasRefresh").on("click",function(){
+	    	getSenhasUtilizador(uniqueID);
+	    })
+	    
+	    $("#listaMinhasSenhas").on("click","li",function(){
+	    	gmaps = $(this).attr("data-gmaps");
+	    	idSenhaPopUp = $(this).attr("data-idSenha");
+	    })
+	    
+	    $("#desistir_minhas_senhas_page").on("click",function(){
+	    	giveUpSenha(idSenhaPopUp);
+		})
+    },
+    
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Callback Success! Result = '+result)
+    },
+    
+    errorHandler:function(error) {
+        alert(error);
+    },
+    
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     }
+	    
 };
