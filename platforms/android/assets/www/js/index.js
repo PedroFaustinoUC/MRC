6c9f1cb18f62d9d4a223f6c89036daa6ab4c3d9a
Pedro Faustino <pfaustino.pt@gmail.com>
2013-10-17 18:25:13 +0100

    Google maps implementado

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -36,8 +36,11 @@ var app = {
 	    //URL base do webservice
 	    //var rootURL = "http://10.3.3.126/mrc/api/";
 	    var rootURL = "http://10.3.1.39/mrc/api/";
-	    var gmaps ="";
 	    var idSenhaPopUp = 0;
+	    var detalhesSenha=[];
+	    var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        var directionDisplay,directionsService = new google.maps.DirectionsService(),map;
 	    //var rootURL = "http://192.168.1.64/mrc/api/";
 	    
 	    /*
@@ -48,18 +51,9 @@ var app = {
 	    function onBackKeyDown() {
 	    	//playBeep()
 	    	//vibrate()
-	    	//navigator.app.exitApp();
+	    	navigator.app.exitApp();
 		};
-		
-		//Alerta que surge quando se desiste de uma senha
-		function showAlertDesistir() {
-	        navigator.notification.alert(
-	            'Senha removida!',  // message
-	            null,         // callback
-	            'Desistir da Senha',   // title
-	            'Ok'                  // buttonName
-	        );};
-	        
+		        
 	    //Evento da ligação à internet perdida
 		function onOffline(){
 			alert("Sem ligação à internet!");
@@ -217,7 +211,7 @@ var app = {
 						lista+= '<li data-role="list-divider">'+senhas.nomeFrontDesk+'</li>';
 						idControlo=senhas.idFrontDesk
 						}
-						lista+=('<li data-gmaps="'+senhas.googleMaps+'" data-idSenha="'+senhas.idSenha+'"><a href="#popupsenha" data-rel="popup"><h2>'+senhas.nomeBalcaoAtendimento+'</h2><p><strong>'+senhas.nomeLocalizacao+'</strong></p><br>');
+						lista+=('<li data-idSenha="'+senhas.idSenha+'"><a href="#popupsenha" data-rel="popup"><h2>'+senhas.nomeBalcaoAtendimento+'</h2><p><strong>'+senhas.nomeLocalizacao+'</strong></p><br>');
 						if((senhas.nSenhaUtilizador-senhas.nSenhaActual)<=5)
 						{
 							lista+=('<p><font color="red"><strong>Senha Actual: </strong>'+senhas.nSenhaActual+'<strong> Hora Prevista: </strong>'+senhas.horaAtendimento+'</font></p>');
@@ -245,7 +239,12 @@ var app = {
 				success: function(data){
 					//$.mobile.changePage("#minhas_senhas_page" );
 					$( "#popupsenha" ).popup( "close" );
-					showAlertDesistir();
+					navigator.notification.alert(
+			            'Senha removida!',  // message
+			            null,         // callback
+			            'Desistir da Senha',   // title
+			            'Ok'                  // buttonName
+			        );
 					$("#minhas_senhas_page").trigger("pageshow");
 				},
 				error: function(jqXHR, textStatus, errorThrown){
@@ -254,7 +253,108 @@ var app = {
 			});
 		    }
 		
+
+		//Webservice - verificar detalhes de uma senha
+	    function getSenhaDetalhes(idsenha){
+		    $.ajax({
+				type: 'GET',
+				url: rootURL+"senhas/"+idsenha+"/detalhes",
+				dataType: "json", // data type of response
+				success: function(data){
+					var list = data == null ? [] : (data.senhas instanceof Array ? data.senhas : [data.senhas]);
+					detalhesSenha=[];
+					$.each(list, function(index, senhas) {
+						detalhesSenha.push(senhas.idSenha);
+						detalhesSenha.push(senhas.dataEmissao);
+						detalhesSenha.push(senhas.horaEmissao);
+						detalhesSenha.push(senhas.nomeFrontDesk);
+						detalhesSenha.push(senhas.nomeLocalizacao);
+						detalhesSenha.push(senhas.nomeBalcaoAtendimento);
+     					detalhesSenha.push(senhas.googleMaps);
+						detalhesSenha.push(senhas.morada);
+						detalhesSenha.push(senhas.nSenhaUtilizador);
+						detalhesSenha.push(senhas.nSenhaActual);
+						detalhesSenha.push(senhas.mediaTempoEspera);
+						detalhesSenha.push(senhas.mediaTempoAtendimento);
+						detalhesSenha.push(senhas.horaAtendimento);
+					});
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('Erro: ' + textStatus);
+				}
+			});
+		    }
 				
+
+		    /*
+		     * Funções associadas ao GPS do telemóvel
+		     */
+			function onSuccessGPS(position) {
+		        $("#from").val("("+position.coords.latitude+","+position.coords.longitude+")");
+                calculateRoute();
+		    }
+
+		    function onErrorGPS(error) {
+		    	navigator.notification.alert(
+			            'Ocorreu um erro ao tentar obter as suas coordenadas.',  // message
+			            null,         // callback
+			            'Erro de GPS',   // title
+			            'Ok'                  // buttonName
+			        );
+		    }
+
+		    /*
+		     * Funções associadas ao Google Maps
+		     */
+		     function initialize() 
+            {
+            	navigator.geolocation.getCurrentPosition(onSuccessGPS, onErrorGPS);
+                directionsDisplay = new google.maps.DirectionsRenderer();
+                var mapCoord= detalhesSenha[6].split(",");
+                var mapCenter = new google.maps.LatLng(mapCoord[0],mapCoord[1]);
+                var myOptions = {
+                    zoom:10,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    center: mapCenter
+                }
+                map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+                directionsDisplay.setMap(map);
+                directionsDisplay.setPanel(document.getElementById("directions")); 
+				$("#to").val(mapCenter);
+            }
+
+            function calculateRoute() 
+            {
+                var selectedMode = $("#mode").val(),
+                    start = $("#from").val(),
+                    end = $("#to").val();
+
+                if(start == '' || end == '')
+                {
+                    // cannot calculate route
+                    $("#results").hide();
+                    return;
+                }
+                else
+                {
+                    var request = {
+                        origin:start, 
+                        destination:end,
+                        travelMode: google.maps.DirectionsTravelMode[selectedMode]
+                    };
+
+                    directionsService.route(request, function(response, status) {
+                        if (status == google.maps.DirectionsStatus.OK) {
+                            directionsDisplay.setDirections(response); 
+                            $("#results").show();
+                        }
+                        else {
+                            $("#results").hide();
+                        }
+                  });
+                }
+            }
+
 
 	    /* ____________
 	     * |IMPORTANTE|
@@ -289,51 +389,84 @@ var app = {
 	    	$( "#balcaoAtendimento" ).selectmenu( "refresh" );
 	    	$("#gerar_senha_page_avancarButton").addClass( "ui-disabled" );
 	    	getLocalizacao($("#frontdesk").val());
-	    })
+	    });
 	    
 	    //Quando o select "Localização" for alterado o select "Balcao Atendimento" fica activo
 	    //Após o select "Balcao Atendimento" ficar activo, são carregados os dados
 	    $("#localizacao").on("change",function(){
 	    	$("#balcaoAtendimento").selectmenu( "enable" );
 	    	getBalcaoAtendimento($("#frontdesk").val(),$("#localizacao").val());
-	    })
+	    });
 	    
 	    //Quando o select "Balcao Atendimento" for alterado o botão "Avançar" fica activo
 	    $("#balcaoAtendimento").on("change",function(){
 	    	$("#gerar_senha_page_avancarButton").removeClass( "ui-disabled" );
-	    })
+	    });
 	    
 	    //Quando o select "Balcao Atendimento" for alterado o botão "Avançar" fica activo
 	    $("#info_gerar_senha_page").on("pagebeforeshow",function(){
 	    	getCurrentSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val());
-	    })
+	    });
 	    
 	    //Quando se clica no botão da senha actual a página é actualizada 
 	    $("#senhaActual").on("click",function(){
 	    	getCurrentSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val());
-	    })
+	    });
 	    
 	    //Quando se clica no botão de gerar ticket é gerada uma nova senha
 	    $("#info_gerar_senha_page_gerarTicketButton").on("click",function(){
 	    	addSenha($("#frontdesk").val(),$("#localizacao").val(),$("#balcaoAtendimento").val(),uniqueID);
-	    })
+	    });
 	    
 	    //Quando a página das senhas é carregada 
-	    $("#minhas_senhas_page").on("pageshow",function(){
+	    $("#minhas_senhas_page").on("pagebeforeshow",function(){
 	    	getSenhasUtilizador(uniqueID);
-	    })
+	    });
 	    
+	    //Quando se clica no botão de Refresh é actualizada a página
 	   $("#minhasSenhasRefresh").on("click",function(){
 	    	getSenhasUtilizador(uniqueID);
-	    })
-	    
+	    });
+
+		//Quando se clica numa das senhas grava-se o seu id e a sua localização do gmaps 
 	    $("#listaMinhasSenhas").on("click","li",function(){
-	    	gmaps = $(this).attr("data-gmaps");
 	    	idSenhaPopUp = $(this).attr("data-idSenha");
-	    })
+	    	getSenhaDetalhes(idSenhaPopUp);
+	    });
 	    
+	    //Quando se confirma a desistencia de uma senha esta é removida
 	    $("#desistir_minhas_senhas_page").on("click",function(){
 	    	giveUpSenha(idSenhaPopUp);
-		})
-    }   
+		});
+
+		//Quando o select "Balcao Atendimento" for alterado o botão "Avançar" fica activo
+	    $("#minha_senha_detalhes_page").on("pagebeforeshow",function(){
+	    	$('#detalhes_senhaActual').empty();
+			$('#detalhes_senhaActual').siblings('.ui-btn-inner').children('.ui-btn-text').text("Senha Actual: "+detalhesSenha[9]);
+			$( "#detalhes_senhaActual" ).buttonMarkup( "refresh" );
+			$('#detalhes_minhaSenha').val(detalhesSenha[8]);
+			$('#detalhes_dataEmissao').val(detalhesSenha[2]+" na data "+detalhesSenha[1]);
+			$('#detalhes_horaPrevista').val(detalhesSenha[12]);
+			$('#detalhes_tempoEspera').val(detalhesSenha[10]);
+			$('#detalhes_tempoAtendimento').val(detalhesSenha[11]);
+			$('#detalhes_frontDesk').val(detalhesSenha[3]);
+			$('#detalhes_localizacao').val(detalhesSenha[4]);
+			$('#detalhes_balcaoAtendimento').val(detalhesSenha[5]);
+			$('#detalhes_morada').val(detalhesSenha[7]);
+	    });
+
+		$("#google_maps").on("pagebeforeshow",function(){
+            initialize() ;
+	    });
+	    
+	    $("#getDirections").on("click",function(){
+            calculateRoute();
+	    });
+
+	    
+
+
+
+	 //@end
+    }
 };
