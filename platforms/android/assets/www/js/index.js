3f3659780cb48d8232b95f52934fcd1ecc9f48e4
Pedro Faustino <pfaustino.pt@gmail.com>
2013-10-18 19:11:36 +0100

    Adição do Push notification (Apenas funciona se o servidor estiver fora do DEI - Firewall problems)

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -26,7 +26,7 @@ var app = {
 	    $.ajaxSetup({ 
 					beforeSend: function() { $.mobile.showPageLoadingMsg(); }, //Show spinner
 					complete: function() { $.mobile.hidePageLoadingMsg() }, //Hide spinner
-					timeout: 5000,
+					timeout: 15000,
 					error: function(  ){
 						navigator.notification.alert(
 				            'Problema ao conectar com o servidor. Tente novamente.',  // message
@@ -516,10 +516,51 @@ var app = {
             calculateRoute();
 	    });
 
-	    
+	    /*
+	     * Notificação
+	     */
+	    var pushNotification = window.plugins.pushNotification;
+		pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"691270257065","ecb":"app.onNotificationGCM"});
 
 
 
 	 //@end
+    },
+
+// result contains any message sent from the plugin call
+successHandler: function(result) {
+    alert('Callback Success! Result = '+result)
+},
+errorHandler:function(error) {
+    alert(error);
+},
+
+onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     }
+
+
+
 };
